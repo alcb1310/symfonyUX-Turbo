c4f8d52fa1254cd438c91b8ef27c6dabf935efc9
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-03-14 22:37:27 -0500

    chapter 34

--- assets/controllers/modal-form_controller.js
+++ assets/controllers/modal-form_controller.js
@@ -5,6 +5,14 @@ export default class extends Controller {
     static targets = ['modal'];
     modal = null;
 
+    connect() {
+        this.element.addEventListener('turbo:submit-end', (event) => {
+            if (event.detail.success) {
+                this.modal.hide();
+            }
+        });
+    }
+
     async openModal(event) {
         this.modal = new Modal(this.modalTarget);
         this.modal.show();
