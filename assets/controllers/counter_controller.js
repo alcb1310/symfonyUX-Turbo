53167c46cb9b1996aca5345cd101f9e985de887e
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-03-19 12:32:10 -0500

    processig streams in a ux controller

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,17 +1,29 @@
 import { Controller } from 'stimulus';
-import { visit } from '@hotwired/turbo';
+import { visit, renderStreamMessage } from '@hotwired/turbo';
 // import * as Turbo from '@hotwired/turbo';
 
 export default class extends Controller {
-  count = 0;
-  static targets = ['count'];
+    count = 0;
+    static targets = ['count'];
 
-  increment() {
-    this.count++;
-    this.countTarget.innerText = this.count;
+    increment() {
+        this.count++;
+        this.countTarget.innerText = this.count;
 
-    if (this.count == 10) {
-      visit('/you-won');
+        const streamMessage = `
+      <turbo-stream action="update" target="flash-container">
+        <template>
+          <div class="alert alert-success">
+            Thanks form clicking ${this.count} times!
+          </div>
+        </template>
+      </turbo>
+    `;
+
+        renderStreamMessage(streamMessage);
+
+        if (this.count == 10) {
+            visit('/you-won');
+        }
     }
-  }
 }
